8e6c299772e91b6b06f45c822ec8d7f5c7160b89
oookawesome <oookawesome@gmail.com>
2020-06-22 22:24:33 +0900

    Add ocr module

---
+++ ocr/ocr.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+import pytesseract
+from pathlib import Path
+from PIL import Image
+
+
+class OCR:
+    @staticmethod
+    def read(img_path: str) -> Optional[str]:
+        _img_path = Path(img_path)
+        if _img_path.suffix in ['.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif']:
+            img = Image.open(_img_path)
+            return pytesseract.image_to_string(img)
+        else:
+            print(f'Not supported file extension! : {_img_path}')
+            return None
+
+
+if __name__ == '__main__':
+    # path = 'resources/test_pdf1.pdf'
+    path = 'resources/test_image1.jpg'
+    ocr = OCR()
+    print(ocr.read(path))
+
---
+++ ocr/setup.md
@@ -0,0 +1,5 @@
+# Installation for Mac (fastest)
+## tesseract 설치
+    $brew install tesseract
+## pip 패키지 설치
+    $pip install pytesseract
